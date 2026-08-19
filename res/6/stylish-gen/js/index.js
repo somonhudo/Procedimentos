8239b6d400917a3a03bc0feae566aa1dcf81f3ad
Rodrigo Schio <rodrigoschio@tutamail.com>
2026-08-19 07:06:34 -0300

    stylish-gen textures

--- res/6/stylish-gen/js/index.js
+++ res/6/stylish-gen/js/index.js
@@ -60,7 +60,7 @@ function geraStilo(randomfrase, cor, fonte, quadro, bg){
 	}
 
 	if(bg) {
-		localStorage.setItem("frase-bg", pickOne(["0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png","13.png","14.png","15.png","16.png","17.png","18","19","none","none","none","none","none"]));
+		localStorage.setItem("frase-bg", parseInt(Math.random()*54)+".png");
 	}
 
 	let bgcolor = localStorage.getItem("frase-bgcolor");
